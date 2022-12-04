fcfab2b52d9ff7ad3a3b4cbc5e2a202e2ec67138
imkss <ksb020@nist.edu>
2022-12-04 16:49:17 +0530

    Basic Mathematical Probs

--- b_Mathematics/D_GCD.java
+++ b_Mathematics/D_GCD.java
@@ -3,8 +3,14 @@ package b_Mathematics;
 public class D_GCD {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println(gcd(24,12));
+		System.out.println(24%12);
 
 	}
-
+	
+	static int gcd(int a, int b) {
+		if (b == 0) return a;
+		return gcd(b, a %b);
+	}
 }
--- b_Mathematics/E_ModuloArithnetics.java
+++ b_Mathematics/E_ModuloArithnetics.java
@@ -1,10 +1,40 @@
-package b_Mathematics;
+ package b_Mathematics;
 
 public class E_ModuloArithnetics {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println(findPower(12, 5));
+		
+		System.out.println(fastPower(1002, 5, 100000009));
 	}
-
+	
+	static int findPower(int a, int b) {
+		int res = 1;
+		
+		while(b > 0) {			
+			if((b & 1) == 1) {
+				res = res * a;
+			}
+			a = a * a;
+			b = b >> 1;			
+		}
+		
+		return res;
+	}
+	
+	static long fastPower(long a, long b, int n ) {
+		long res = 1;
+		
+		while(b > 0) {			
+			if((b & 1) == 1) {
+				res = (res % n * a % n) %n;
+			}
+			a = (a % n * a % n) % n;
+			b = b >> 1;			
+		}
+		
+		return res;
+	}
+	
 }

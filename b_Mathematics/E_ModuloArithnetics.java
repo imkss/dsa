65a9cdecd79ab711e2cdab799dc5aa2a0d26ed13
imkss <ksb020@nist.edu>
2022-12-06 21:53:34 +0530

    Some more recurssiove approch

--- b_Mathematics/E_ModuloArithnetics.java
+++ b_Mathematics/E_ModuloArithnetics.java
@@ -4,24 +4,10 @@ public class E_ModuloArithnetics {
 
 	public static void main(String[] args) {
 		
-		System.out.println(findPower(12, 5));
-		
-		System.out.println(fastPower(1002, 5, 100000009));
+		System.out.println(fastPower(1002, 5, 100000007));
 	}
 	
-	static int findPower(int a, int b) {
-		int res = 1;
-		
-		while(b > 0) {			
-			if((b & 1) == 1) {
-				res = res * a;
-			}
-			a = a * a;
-			b = b >> 1;			
-		}
-		
-		return res;
-	}
+
 	
 	static long fastPower(long a, long b, int n ) {
 		long res = 1;
---
+++ c_Recursion/F_powerSet.java
@@ -0,0 +1,19 @@
+package c_Recursion;
+
+public class F_powerSet {
+
+	public static void main(String[] args) {
+		
+		PowerSet("123", 0, "");
+	}
+	
+	static void PowerSet(String str, int i, String cur) {
+		if(i == str.length()) {
+			System.out.print(cur + " ");
+			return;
+		}
+		
+		PowerSet(str, i+1, cur + str.charAt(i));
+		PowerSet(str, i+1, cur);
+	}
+}

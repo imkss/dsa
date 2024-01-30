3a7f43f7a8920159d4ad4056050fc64d7c5b1707
Sunny <ksb020@nist.edu>
2024-01-30 13:39:52 +0530

    A2Z "String"

---
+++ A2Z/Strings/ReverseWordsString.java
@@ -0,0 +1,18 @@
+package A2Z.Strings;
+
+public class ReverseWordsString {
+    public static void main(String[] args) {
+        String s =  "the sky is blue";
+        System.out.println(fnc(s));
+    }
+
+    public static String fnc(String str) {
+       String[] s = str.split(" ");
+       StringBuilder rev = new StringBuilder();
+       for(int i = s.length - 1; i >= 0; i--) {
+           rev.append(s[i]);
+           rev.append(" ");
+       }
+       return rev.toString().trim();
+    }
+}

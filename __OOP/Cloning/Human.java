dc5ff2a7abaec27cb91ad552862c59092a5a2a94
Sunny <ksb020@nist.edu>
2024-02-13 01:59:02 +0530

    Cloning - The Deep Copy

--- __OOP/Cloning/Human.java
+++ __OOP/Cloning/Human.java
@@ -11,7 +11,20 @@ public class Human implements Cloneable{
         this.arr = new int[]{1,2,3,4,5};
     }
 
+//    public Object clone() throws CloneNotSupportedException{
+//    //SHALLOW COPY
+//        return super.clone();
+//    }
+
     public Object clone() throws CloneNotSupportedException{
-        return super.clone();
+//    DEEP COPY
+        Human twin= (Human) super.clone(); //This is actually shallow copy
+
+//        Make a DEEP COPY
+        twin.arr = new int[twin.arr.length];
+        for(int i = 0; i < twin.arr.length; i++) {
+            twin.arr[i] = this.arr[i];
+        }
+        return twin;
     }
 }
--- __OOP/Cloning/Main.java
+++ __OOP/Cloning/Main.java
@@ -7,14 +7,16 @@ public class Main {
         Human sunny = new Human(22, "Sunny");
 //        Human twin = new Human(sunny);
         Human twin = (Human) sunny.clone();
-        System.out.println(twin.age + " "+ twin.name);
-        System.out.println(Arrays.toString(twin.arr));
+//        System.out.println(twin.age + " "+ twin.name);
+//        System.out.println(Arrays.toString(twin.arr));
 
         twin.arr[0] = 100;
         twin.age = 18;
+        System.out.println(Arrays.toString(twin.arr));
+        System.out.println(Arrays.toString(sunny.arr));
 
         System.out.println(sunny.age);
 
-        System.out.println(Arrays.toString(sunny.arr)); //For Non-primitive data type it just reference to the same object. Known as shallow copy. To solve this we use Deep Copy
+//        System.out.println(Arrays.toString(sunny.arr)); //For Non-primitive data type it just reference to the same object. Known as shallow copy. To solve this we use Deep Copy
     }
 }

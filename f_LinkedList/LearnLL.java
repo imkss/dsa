390f0f040028462a58617b0fd59292bd80491204
Sunny <ksb020@nist.edu>
2024-05-21 23:32:15 +0530

    Learning Linked List

---
+++ f_LinkedList/LearnLL.java
@@ -0,0 +1,24 @@
+package f_LinkedList;
+class Node {
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+}
+public class LearnLL {
+    public static void main(String[] args) {
+        Node x = new Node(5);
+        Node y = new Node(10, x);
+        System.out.println(y.next);
+
+
+    }
+}

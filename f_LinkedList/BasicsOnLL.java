f3195db365f18257b01cd77c125067b334516d94
Sunny <ksb020@nist.edu>
2024-05-23 23:43:58 +0530

    Deleting the tail of the LL

--- f_LinkedList/BasicsOnLL.java
+++ f_LinkedList/BasicsOnLL.java
@@ -1,6 +1,6 @@
 package f_LinkedList;
 public class BasicsOnLL {
-    public static Node Arr2LL(int[] arr) {
+    public static Node arr2LL(int[] arr) {
     Node head = new Node(arr[0]);
     Node mover = head;
     for(int i = 1; i < arr.length; i++) {
@@ -11,7 +11,15 @@ public class BasicsOnLL {
     return head;
     }
 
-    public static int LengthOfLL(Node head) {
+    public static void printLL (Node head) {
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static int lengthOfLL(Node head) {
         int cnt = 0;
         Node temp = head;
         while(temp != null){
@@ -21,7 +29,7 @@ public class BasicsOnLL {
         return cnt;
     }
 
-    public static boolean SearchInLL(Node head, int num) {
+    public static boolean searchInLL(Node head, int num) {
         Node temp = head;
         while(temp != null) {
             if(temp.data == num) return true;
@@ -30,21 +38,30 @@ public class BasicsOnLL {
         return false;
     }
 
-    public static void main(String[] args) {
-        int[] arr = {19, 34, 1, 76, 9, 21};
-        Node head = Arr2LL(arr);
-//        System.out.println(head.next);
+    public static Node removeHead(Node head) {
+        if(head == null) return head;
+        head = head.next;
+        return head;
+    }
 
+    public static Node removeTail(Node head){
+        if(head == null || head.next == null) return null;
         Node temp = head;
-        while(temp != null) {
-//            System.out.println(temp.data + ", " + temp.next);
+        while(temp.next.next != null) {
             temp = temp.next;
         }
+        temp.next = null;
+        return head;
+    }
 
-       int len = LengthOfLL(head);
-//        System.out.println(len);
-
-        System.out.println(SearchInLL(head, 18));
+    public static void main(String[] args) {
+        int[] arr = {19, 34, 1, 76, 9, 21};
+        Node head = arr2LL(arr);
+//        System.out.println(head.next);
+//        System.out.println(lengthOfLL(head));
+//        System.out.println(searchInLL(head, 18));
+//        printLL(removeHead(head));
+        printLL(removeTail(head));
     }
 
 }

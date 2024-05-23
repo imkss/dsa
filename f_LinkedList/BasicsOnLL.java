b9fbee9aafe9a647673a170b8d17b97cf3e2471a
Sunny <ksb020@nist.edu>
2024-05-24 00:10:48 +0530

    Deleting the kth element from the LL

--- f_LinkedList/BasicsOnLL.java
+++ f_LinkedList/BasicsOnLL.java
@@ -54,6 +54,27 @@ public class BasicsOnLL {
         return head;
     }
 
+    public static Node removeK(Node head, int k) {
+        if(head == null) return head;
+        if(k == 1) {
+            head = head.next;
+            return head;
+        }
+        int cnt = 0;
+        Node temp = head;
+        Node prev = null;
+        while(temp != null) {
+            cnt++;
+            if(cnt == k) {
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
         int[] arr = {19, 34, 1, 76, 9, 21};
         Node head = arr2LL(arr);
@@ -61,7 +82,8 @@ public class BasicsOnLL {
 //        System.out.println(lengthOfLL(head));
 //        System.out.println(searchInLL(head, 18));
 //        printLL(removeHead(head));
-        printLL(removeTail(head));
+//        printLL(removeTail(head));
+        printLL(removeK(head, 9));
     }
 
 }

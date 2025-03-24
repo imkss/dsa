6065abe74c209b2660ad36fba01a7f983aed0179
Sunny <ksb020@nist.edu>
2025-03-24 23:35:24 +0530

    done for the eve :)

--- _one_shot/BubbleSort.java
+++ _one_shot/BubbleSort.java
@@ -3,18 +3,17 @@ package _one_shot;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] nums = {6,15,1,32,18,9,74,11};
+        int[] nums = {6, 15, 1, 32, 18, 9, 74, 11};
 
         System.out.println("Before sorting:: ");
-        for(int num : nums) {
+        for (int num : nums) {
             System.out.print(num + " ");
         }
 
         sortValues(nums);
-
         System.out.println();
         System.out.println("After sorting:: ");
-        for(int num : nums){
+        for (int num : nums) {
             System.out.print(num + " ");
         }
     }
@@ -22,17 +21,17 @@ public class BubbleSort {
     public static int[] sortValues(int[] arr) {
         int temp = 0;
 
-        for(int i = 0; i < arr.length; i++) {
-           for(int j = 0; j < arr.length - 1; j++) {
-               if(arr[j] > arr[j+1]){
-                   temp = arr[j];
-                   arr[j] = arr[j+1];
-                   arr[j+1] = temp;
-               }
-           }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
 // Just to check the values after each iteration :)
             System.out.println();
-            for(int num : arr){
+            for (int num : arr) {
                 System.out.print(num + " ");
             }
         }
--- _one_shot/SelectionSort.java
+++ _one_shot/SelectionSort.java
@@ -3,7 +3,7 @@ package _one_shot;
 public class SelectionSort {
 
     public static void main(String[] args) {
-        int[] nums = {6,15,1,32,18,9,74,11};
+        int[] nums = {17,15,1,32,18,9,74,11};
 
         System.out.println("Before sorting:: ");
         for(int num : nums) {
@@ -20,7 +20,24 @@ public class SelectionSort {
     }
 
     public static int[] sortValues(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if(arr[minIndex] > arr[j]){
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
 
-    return arr;
+            // Just to check the values after each iteration :)
+            System.out.println();
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+
+        }
+        return arr;
     }
-    }
+}
